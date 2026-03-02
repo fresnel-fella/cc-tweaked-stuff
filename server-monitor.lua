18b3fd96c78c4e2a16c2840be7d545d3920d47a2
fresnel-fella <alexmatyaszek@proton.me>
2026-03-02 18:21:56 +0000

    shadows i htink

--- server-monitor.lua
+++ server-monitor.lua
@@ -5,7 +5,7 @@ print("starting monitor")
 local screen_drawing = netrequire("screen_drawing")
 local x_increment,y_increment = screen_drawing.get_xy_increment()
 function draw_box(x,y,sx,sy)
-    screen_drawing.draw(x,y,sx,sy," ",nil,colors.gray)
+    screen_drawing.draw(x,y,sx,sy," ",nil,colors.lightGray)
     screen_drawing.draw(x,y,0,sy," ",nil,colors.white,0,0,1,0)
     screen_drawing.draw(x,y,sx,0," ",nil,colors.white,0,0,0,1)
     screen_drawing.draw(x+sx,y,0,sy," ",nil,colors.black,-1,0,1,0)
@@ -14,7 +14,7 @@ end
 while true do
     screen_drawing.draw(0,0,1,1,"h",colors.cyan,colors.blue)
     draw_box(0.05,0.05,0.9,0.9)
-    screen_drawing.draw_text_centred("hello world!",0.5,0.5,colors.black,colors.white)
+    screen_drawing.draw_text_centred("hello world!",0.5,0.5,colors.black,colors.lightGray)
     screen_drawing.render()
     sleep(1)
 end
