45ee5668533407a14ce95a494246066ca84b9991
fresnel-fella <alexmatyaszek@proton.me>
2026-03-03 21:11:14 +0000

    h

--- image_display.lua
+++ image_display.lua
@@ -8,6 +8,7 @@ end
 function get_byte(s,i)
     return string.byte(string.sub(s,i,i))
 end
+monitor.setTextScale(0.5)
 local image = netrequire("output.ccp") -- ccp is not short for chinese communist party it is short for computer craft picture
 local width = get_byte(image,1) + get_byte(image,2)*2^8 + get_byte(image,3)*2^16 + get_byte(image,4)*2^24
 local height = get_byte(image,1+4) + get_byte(image,2+4)*2^8 + get_byte(image,3+4)*2^16 + get_byte(image,4+4)*2^24
