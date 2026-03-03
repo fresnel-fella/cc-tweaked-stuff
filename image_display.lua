71cbd7118fd01d70bc678789843c845c6c083cc9
fresnel-fella <alexmatyaszek@proton.me>
2026-03-03 21:04:11 +0000

    h

--- image_display.lua
+++ image_display.lua
@@ -19,7 +19,7 @@ for x = 1,width do
     for y = 1,height do
         local byte = get_byte(image,i)
         local primary = (byte) % 16
-        local secondary = ((byte-primary) / (2^4))
+        local secondary = ((byte-primary) / (2^3))
         primary = 2^(primary)
         secondary = 2^(primary)
         print(primary)
