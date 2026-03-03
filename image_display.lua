f24a3ac7430d3ea131e9bcf6cf7802215dc603ad
fresnel-fella <alexmatyaszek@proton.me>
2026-03-03 20:00:43 +0000

    image processing

--- image_display.lua
+++ image_display.lua
@@ -10,3 +10,19 @@ local width = get_byte(image,1) + get_byte(image,2)*2^8 + get_byte(image,3)*2^16
 local height = get_byte(image,1+4) + get_byte(image,2+4)*2^8 + get_byte(image,3+4)*2^16 + get_byte(image,4+4)*2^24
 print(width)
 print(height)
+local i = 9
+monitor.clear()
+for x = 1,width do
+    for y = 1,height do
+        local byte = get_byte(image,i)
+        local primary = math.max(byte,15)
+        local secondary = (byte-primary) >> 4
+        primary = 1 << (primary-1)
+        secondary = 2 << (secondary-1)
+        local primary_color = colors[primary]
+        local secondary_color = colors[secondary]
+        monitor.setCursorPos(x,y)
+        monitor.blit(string.char(127),colors.toBlit(primary_color),colors.toBlit(secondary_color))
+        i = i + 1
+    end
+end
