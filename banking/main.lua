cbd1b3916e4fcaa0ea9e281b369f761a4b887e9f
fresnel-fella <alexmatyaszek@proton.me>
2026-03-05 20:30:43 +0000

    banking stuff

--- banking/main.lua
+++ banking/main.lua
@@ -1,1 +1,7 @@
+function netrequire(file_name)
+    local out,error = load(http.get("https://raw.githubusercontent.com/fresnel-fella/cc-tweaked-stuff/refs/heads/main/"..file_name).readAll())
+    if error then error(error) end
+    return out
+end
 print("starting banking...")
+netrequire("banking/encryption.lua")(_G)
