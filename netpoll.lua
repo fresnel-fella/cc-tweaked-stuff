99ba4f3cbb83235d9a7744c49169346e623942f5
fresnel-fella <alexmatyaszek@proton.me>
2026-03-31 15:07:39 +0100

    typo

--- netpoll.lua
+++ netpoll.lua
@@ -11,7 +11,7 @@ end
 function netpoll:poll()
     local stop = false
     repeat
-        local id,msg,prot = rednet.recieve()
+        local id,msg,prot = rednet.receive()
         if not msg then stop = true else
             table.insert(self.reci_requests,{id,msg,prot})
         end
